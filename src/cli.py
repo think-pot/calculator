39289a24fb2727889ce6ca66e9bbb1b0819cd978
think-pot <vikram18.g@gmail.com>
2026-06-14 18:17:41 +0530

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -19,6 +19,10 @@ def calculate(operation, num1, num2=None):
             result = multiply(num1, num2)
         elif operation == "divide":
             result = divide(num1, num2)
+        elif operation == "power":
+            result = power(num1, num2)
+        elif operation == "square_root" or operation== "sqrt":
+            result = square_root(num1)
         else:
             click.echo(f"Unknown operation: {operation}")
             sys.exit(1)
